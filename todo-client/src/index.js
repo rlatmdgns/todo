93b0d71458a38b5d07a708ff08fbcfa5b7518653
kimseunghun <rlatmdgns3076@gmail.com>
2020-10-21 08:25:00 +0900

    리덕스 공부 중

--- todo-client/src/index.js
+++ todo-client/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import "./index.css";
 import App from "./App";
 
